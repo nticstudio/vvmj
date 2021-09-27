a2062294b6772b6dde07c650360d269f69c87263
phpstrom <phpstorm>
2021-09-27 15:49:35 +0200

    Install HCL  Mysql + upgrade  SF5.3

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
